5dc5fff9df5960e13c5d163c979fcf2246b03a48
alpha_devking <danielose266@gmail.com>
2022-08-31 20:40:32 +0100

    New commit

--- src/Components/NavArea.js
+++ src/Components/NavArea.js
@@ -4,6 +4,8 @@ import Logo from '../Images/nftx-apocalypse.png'
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
+import { BsFillBrightnessHighFill } from 'react-icons/bs';
+import Stack from 'react-bootstrap/Stack';
 
 function NavArea() {
   return (
@@ -11,18 +13,18 @@ function NavArea() {
       <Navbar bg="dark" variant="dark">
         <Container fluid>
           <Navbar.Brand href="#home"
-          className="container-fluid w-100">
+          className="">
             <img
               alt=""
               src={Logo}
               width="50"
               height="50"
-              className="d-inline-block align-top rounded-5"
+              className="d-inline-block rounded-5"
             />{' '}
             NFT Apocalypse
           </Navbar.Brand>
           <Navbar.Brand>
-          <Button variant="light">Night</Button>{' '}
+          <Button variant="outline-light"><BsFillBrightnessHighFill /></Button>{''}
           </Navbar.Brand>
         </Container>
       </Navbar>
