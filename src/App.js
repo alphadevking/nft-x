ff9d1d03bdb649d396fcc76684d8edf0323d83a8
alpha_devking <danielose266@gmail.com>
2022-08-31 21:43:09 +0100

    New commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 // import logo from './logo.svg';
 // import './App.css';
-import BasicExample from './Components/BasicExample';
 import NavArea from './Components/NavArea';
 
 function App() {
   return (
     <div className="App">
-      {/* <BasicExample /> */}
       <NavArea />
     </div>
   );
--- src/Components/NavArea.js
+++ src/Components/NavArea.js
@@ -8,9 +8,31 @@ import { BsFillBrightnessHighFill } from 'react-icons/bs';
 import Stack from 'react-bootstrap/Stack';
 
 function NavArea() {
+
+  const changeTheme = () => {
+    // let theToggle = document.getElementById("change-color");
+    // theToggle.toggleStatus = "on";
+    // switch (theToggle.toggleStatus) {
+    //   case "on":
+    //     theToggle.toggleStatus = "off";
+    //     theToggle.style.color = "purple";
+    //     theToggle.style.backgroundColor = "yellow";
+    //     break;
+    //   case "off":
+    //     theToggle.toggleStatus = "on";
+    //     theToggle.style.color = "yellow";
+    //     theToggle.style.backgroundColor = "purple";
+    //     break;
+    // }
+
+    this.state = {
+      theme: alert('hello')
+    }
+
+  }
   return (
     <>
-      <Navbar bg="dark" variant="dark">
+      <Navbar bg="dark" variant="dark" className='position-fixed w-100 top'>
         <Container fluid>
           <Navbar.Brand href="#home"
           className="">
@@ -24,7 +46,7 @@ function NavArea() {
             NFT Apocalypse
           </Navbar.Brand>
           <Navbar.Brand>
-          <Button variant="outline-light"><BsFillBrightnessHighFill /></Button>{''}
+          <Button variant="outline-light" id='change-theme' onClick={() =>changeTheme()}><BsFillBrightnessHighFill /></Button>{''}
           </Navbar.Brand>
         </Container>
       </Navbar>
